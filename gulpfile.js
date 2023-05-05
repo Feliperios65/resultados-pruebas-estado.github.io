6c7e8e741156849e58af4c3778103f669427b5ba
Felipe Rios <feliperios665@gmail.com>
2023-05-05 17:38:08 -0500

    creando funcion para procesar java script

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 //compilar sass
-const{src, dest} = require("gulp");
+const{src, dest, parallel} = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber")
 function css(done){
@@ -13,7 +13,14 @@ function css(done){
 const {watch} = require("gulp")
 function mirar(done){
     watch("src/sass/**/*.scss",css)
+    watch("src/js/**/*.js",javascript)
+    done();
+}
+function javascript(done){
+    src("src/js/app.js")
+        .pipe(dest("build/js"))
     done();
 }
 exports.css = css;
-exports.mirar = mirar;
+exports.mirar = parallel(mirar, javascript);
+exports.javascript = javascript;
