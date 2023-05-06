0101de40302e0e00448dbc82bcfde06132cdd4b5
Felipe Rios <feliperios665@gmail.com>
2023-05-05 19:51:53 -0500

    mejorando diseño y añadiendo funcionalidad

--- build/js/app.js
+++ build/js/app.js
@@ -1,1 +1,9 @@
-document.querySelector(".icono");
+function promedio(){
+    let inputs = document.querySelectorAll("div.campo > input");
+    let puntaje = document.querySelector('.score');
+    let suma = 0;
+    inputs.forEach(function(e){
+        suma = suma + parseInt(e.value);
+    })
+    puntaje.textContent = suma;
+}
