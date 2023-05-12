29d83751fde679b24686173f5f7d89431a6ac37f
Felipe Rios <feliperios665@gmail.com>
2023-05-12 15:49:32 -0500

    funcion en java script terminada

--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,16 @@ function promedio(){
     let inputs = document.querySelectorAll("div.campo > input");
     let puntaje = document.querySelector('.score');
     let suma = 0;
-    inputs.forEach(function(e){
-        suma = suma + parseInt(e.value);
+    inputs.forEach(function(e,i){
+        let entero = parseInt(e.value)
+        if (isNaN(entero) == false){
+            suma = suma + entero; 
+            puntaje.textContent = suma; 
+        }
+        else{
+            alert("Solo se deben digitar numeros.");
+            puntaje.textContent = 0;
+        }
+        e.value= ""
     })
-    puntaje.textContent = suma;
 }
